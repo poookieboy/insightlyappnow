f37a51b3d7b21bf3a8f1d1d95643e36af1bbd9e8
poookieboy <khybastan@gmail.com>
2026-07-11 21:37:03 +0300

    Create supabase.ts

---
+++ src/lib/supabase.ts
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient(
+  "https://xprysgjlwouonpylgusb.supabase.co",
+  "sb_publishable_Cdh8umqD_Z49bAJ2VcW2nQ_OXDJhMZ6"
+);
