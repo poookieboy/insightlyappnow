58b0560e544197bf86df99b0f77a8f12bc83e19b
poookieboy <khybastan@gmail.com>
2026-09-10 19:30:29 +0300

    Remove prerender settings and build configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,10 @@ export default defineConfig({
   tanstackStart: {
     spa: {
       enabled: true,
-
       prerender: {
-        enabled: false,
         crawlLinks: false,
         retryCount: 0,
       },
     },
-  },
-
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
   },
 });
